5ebdfc481b0bc90a7a076d9aa3e4eeb02f8a9603
Leo Medellin <josue.l.medellin@icloud.com>
2025-01-27 09:06:04 -0800

    added and preloaded assets

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -1,3 +1,18 @@
 class Menu extends Phaser.Scene {
-    
+    constructor() {
+        super("menuScene")
+    }
+    preload() {
+        //load images and sprites
+        this.load.image('platform', './assets/platform.png')
+        this.load.image('background', './assets/background.png')
+        //load spritesheet
+        this.load.spritesheet('hero', './assets/hero.png', {
+            frameWidth: 30,
+            frameHeight: 30,
+            startFrame: 0,
+            endFrame: 1
+        })
+    }
+
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,3 +1,10 @@
 class Play extends Phaser.Scene {
-    
+    constructor() {
+        super("playScene")
+    }
+   
+    create() {
+        //place scrolling tile sprite
+        this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0,0)
+    }
 }
